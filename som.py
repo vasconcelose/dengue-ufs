72e49b7022254ad17f2d4631148939cd04de1cfb
Eduardo Vasconcelos <vasconcelos.esm@gmail.com>
2017-01-29 19:58:51 -0200

    acabei som

--- som.py
+++ som.py
@@ -4,6 +4,7 @@ from somut import *
 from pandas import *
 from pandasql import *
 from numpy import *
+from matplotlib.pyplot import *
 
 
 """ preparacao dos dados de entrada da rede """
@@ -14,12 +15,23 @@ csvDengue = 'dados/dados-fixed.csv'
 # selecao de dados de entrada da som (x)
 dfDengue = read_csv(csvDengue)
 
+# transformacao de dados para possibilitar separacao
+dfDengue.medicos_mil_hab = exp(dfDengue.medicos_mil_hab)
+dfDengue.leitos_mil_hab = exp(dfDengue.leitos_mil_hab)
+dfDengue.taxa_obito_hemorragica = exp(\
+	dfDengue.taxa_obito_hemorragica)
+dfDengue.dengue_cem_mil_hab =\
+	exp(dfDengue.dengue_cem_mil_hab / 100)
+
 # anos com dados 100% disponiveis: 94 a 2013
 q = """
 	SELECT * FROM dfDengue
 		WHERE
 			ano >= 1994 AND
-			ano <= 2013
+			ano <= 2013 AND (
+				uf = 'SP' OR
+				uf = 'PA'
+				)
 	"""
 
 # dados de entrada da som
@@ -40,21 +52,38 @@ x = asarray(x)
 
 """ configuracao dos parametros da rede """
 
-s = 1.0 # desvio da gaussiana
-n = 0.02 # taxa de aprendizado
+so = 0.02 # desvio inicial da gaussiana
+no = 0.03 # taxa de aprendizado inicial
 ts = 0.5 # cte de tempo do desvio
 tn = 0.5 # cte de tempo da taxa de aprendizado
-l = 10 # lado da rede
+l = 20 # lado da rede
 dim = 4 # numero de dimensoes das entradas
 w = random.rand(l, l, dim) # pesos sinapticos
-N = 1 # numero maximo de epocas de treinamento
-m = 0.001 # minimo admissivel de alteracao nos pesos
+N = 100 # numero maximo de epocas de treinamento
 
 """"""
 
 
 """ treinamento """
+# x = [ [-1, -1], [1, 1], [-2.5, -2.5], [3.0, 3.0] ]
+# dim = 2
+# w = random.rand(l, l, dim)
+w = treinar(x, so, no, ts, tn, l, dim, w, N)
 
-w = treinar(x, s, n, ts, tn, l, dim, w, N, m)
+img = zeros((l, l))
+
+for k in range(0, len(x)):
+	d = zeros((l, l))
+	
+	for i in range(0, l):
+		for j in range(0, l):
+			d[i, j] = sqrt(sum(pow(x[k] - w[i, j], 2)))
+	
+	i, j = unravel_index(d.argmin(), d.shape)
+	img[i, j] = img[i, j] + 1
+
+imshow(img)
+show()
+clf()
 
 """"""
--- somut.py
+++ somut.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
 from numpy import *
+from random import shuffle
+from os import *
+
+# atualiza valor de s
+def novos(so, epoca, ts):
+	return so * exp(- epoca / ts)
+
+# atualiza valor de n
+def novon(no, epoca, tn):
+	return no * exp(- epoca / tn)
 
 # retorna a vizinhanca topologica baseada na matriz de
 # distancias d e no desvio padrao da gaussiana
-def viztopologica(d, s, l):
-	h = zeros((l, l))
+def viztopologica(d, s, l, dim):
+	h = zeros((l, l, dim))
 	for i in range(0, l):
 		for j in range(0, l):
 			h[i, j] = exp(- d[i, j]**2 /\
@@ -15,8 +25,8 @@ def viztopologica(d, s, l):
 
 # retorna a distancia euclidiana entre o vencedor e
 # os demais neuronios do reticulo
-def distancias(iMin, jMin, l):
-	d = zeros((l, l))
+def distancias(iMin, jMin, l, dim):
+	d = zeros((l, l, dim))
 	for i in range(0, l):
 		for j in range(0, l):
 			d[i, j] = sqrt(pow(i - iMin, 2) +\
@@ -36,17 +46,37 @@ def vencedor(x, w, l):
 	return unravel_index(v.argmin(), v.shape)
 
 # coordena treinamento da som
-def treinar(x, s, n, ts, tn, l, dim, w, N, m):
+def treinar(x, so, no, ts, tn, l, dim, w, N):
 	nEx = len(x)
 	epoca = 1
+	s = so
+	n = no
+
 	while(epoca <= N):
+
+		shuffle(x)
+
 		for ex in range(0, nEx):
+
+			# atualizar console
+			system('clear')
+			print('Epoca {}\nExemplar {}/{}'.format(epoca,\
+				ex, nEx))
+
 			# competicao
 			iMin, jMin = vencedor(x[ex], w, l)
 
 			# cooperacao
-			d = distancias(iMin, jMin, l)
-			h = viztopologica(d, s, l)
+			d = distancias(iMin, jMin, l, dim)
+			h = viztopologica(d, s, l, dim)
 
 			# adaptacao
-			
+			w = w + n * h * (x[ex] - w)
+
+			# atualiza parametros da som
+			s = novos(so, epoca, ts)
+			n = novon(no, epoca, tn)
+
+		epoca = epoca + 1
+
+	return w
